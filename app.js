36bebde94454566ea3b80536b85a00f0a37d37b4
Esten <estengrove99@gmail.com>
2019-01-26 14:49:41 -0700

    Inital Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+/******************************************
+ *  Author : Esten Grove   
+ *  Created On : Sat Jan 26 2019
+ *  File : app.js
+ *******************************************/
+const links = document.querySelectorAll('.link');
+let target = '';
